8a345e0d4c1b2d9c334e56a7e6ba434d464558e6
pekaeltempatkita-cell <pkltempatkita@gmail.com>
2026-09-25 10:32:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://brainscan2.ai.studio";
+const API_BASE_URL = "https://brainscan2-1.ai.studio";
 
 const GOOGLE_CLIENT_ID =
   "26382086135-9h3cprlq9nkimqcdmu37ko8qibqd34vh.apps.googleusercontent.com";
